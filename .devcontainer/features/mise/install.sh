e12004e49a9acee6ee71ec90f3273e86d3b6eb76
Syati <mizuki-y@syati.info>
2024-08-30 07:39:48 +0000

    Set XDG_XXXXX ENV in containerEnv
    Because JetBrains attaches the envs after attached

--- .devcontainer/features/mise/install.sh
+++ .devcontainer/features/mise/install.sh
@@ -12,5 +12,8 @@ su ${USERNAME} -c "echo '$bash_mise' >> ~/.bashrc"
 zsh_mise='eval "$(~/.local/bin/mise activate zsh)"'
 su ${USERNAME} -c "echo '$zsh_mise' >> ~/.zshrc"
 
-
+mkdir -p /.jbdevcontainer/data && chmod -R 777 /.jbdevcontainer
 su ${USERNAME} -c bash -c "~/.local/bin/mise install ruby@${RUBY_VERSION}"
+
+su ${USERNAME} -c "mkdir -p ~/.local/share"
+su ${USERNAME} -c "ln -s /.jbdevcontainer/data/mise ~/.local/share/mise"
